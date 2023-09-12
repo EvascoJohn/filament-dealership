4f701e2d94a64eb4b98182909053bbe48acdab02
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-12 09:45:40 +0800

    intial UI for customer page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/login', function(){
+    return redirect('testpanel');
 })->name('login');
